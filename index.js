f02a69ecf14a24cd7166832e7dd3556fec9f46f6
Dan Ryan <danryanfl@yahoo.com>
2015-06-09 16:22:22 -0400

    add .gitignore

---
+++ index.js
@@ -0,0 +1,17 @@
+var _ = require('lodash');
+
+// Function takes a filename string (without extension) and removes/replaces characters
+// that are not appropriate for filenames
+module.exports = function(dirtyString) {
+  let cleanString = dirtyString.replace(/\./g, '');
+  cleanString = cleanString.replace(/,/g, '');
+  cleanString = cleanString.replace(/&/g, 'and');
+  cleanString = cleanString.replace(/\//g, '-');
+  cleanString = cleanString.replace(/'/g, '');
+  cleanString = cleanString.replace(/\s{2,}/, ' ');
+  cleanString = cleanString.replace('"', "''");
+  cleanString = cleanString.replace('”', "''");
+  cleanString = _.trim(cleanString);
+  cleanString = _.deburr(cleanString);
+  return cleanString;
+};
