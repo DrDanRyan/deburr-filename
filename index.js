025c19ba0ef02389940cd262430e56945968cd1d
Dan Ryan <danryanfl@yahoo.com>
2015-06-09 17:31:55 -0400

    remove let keyword

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 // Function takes a filename string (without extension) and removes/replaces characters
 // that are not appropriate for filenames
 module.exports = function(dirtyString) {
-  let cleanString = dirtyString.replace(/\./g, '');
+  var cleanString = dirtyString.replace(/\./g, '');
   cleanString = cleanString.replace(/,/g, '');
   cleanString = cleanString.replace(/&/g, 'and');
   cleanString = cleanString.replace(/\//g, '-');
